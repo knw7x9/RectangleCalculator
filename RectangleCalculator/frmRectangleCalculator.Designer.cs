abff429b6093f0167f97415a978897ef0b855dfb
Katherine Wilsdon <katherinewilsdon@gmail.com>
2018-09-07 10:08:56 -0600

    Rectangle Calculator
    Calculates the area, perimeter, and diagonal length of a rectangle from the inputted length and width.

---
+++ RectangleCalculator/frmRectangleCalculator.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace RectangleCalculator {
+    partial class frmRectangleCalculator {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblLengthOfRectangle = new System.Windows.Forms.Label();
+            this.lblWidthOfRectangle = new System.Windows.Forms.Label();
+            this.txtLengthOfRectangle = new System.Windows.Forms.TextBox();
+            this.txtWidthOfRectangle = new System.Windows.Forms.TextBox();
+            this.lblOutputPerimeter = new System.Windows.Forms.Label();
+            this.lblOutputArea = new System.Windows.Forms.Label();
+            this.lblOuputDiagonalLength = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblLengthInCentimeters = new System.Windows.Forms.Label();
+            this.lblWidthInCentimeters = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.lblDiagonalLength = new System.Windows.Forms.Label();
+            this.lblAreaInCentimeters = new System.Windows.Forms.Label();
+            this.lblPerimeterInCentimeters = new System.Windows.Forms.Label();
+            this.lblDiagonalLengthInCentimeters = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLengthOfRectangle
+            // 
+            this.lblLengthOfRectangle.AutoSize = true;
+            this.lblLengthOfRectangle.Location = new System.Drawing.Point(91, 30);
+            this.lblLengthOfRectangle.Name = "lblLengthOfRectangle";
+            this.lblLengthOfRectangle.Size = new System.Drawing.Size(43, 13);
+            this.lblLengthOfRectangle.TabIndex = 0;
+            this.lblLengthOfRectangle.Text = "Length:";
+            this.lblLengthOfRectangle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblWidthOfRectangle
+            // 
+            this.lblWidthOfRectangle.AutoSize = true;
+            this.lblWidthOfRectangle.Location = new System.Drawing.Point(97, 63);
+            this.lblWidthOfRectangle.Name = "lblWidthOfRectangle";
+            this.lblWidthOfRectangle.Size = new System.Drawing.Size(38, 13);
+            this.lblWidthOfRectangle.TabIndex = 1;
+            this.lblWidthOfRectangle.Text = "Width:";
+            this.lblWidthOfRectangle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtLengthOfRectangle
+            // 
+            this.txtLengthOfRectangle.Location = new System.Drawing.Point(145, 27);
+            this.txtLengthOfRectangle.Name = "txtLengthOfRectangle";
+            this.txtLengthOfRectangle.Size = new System.Drawing.Size(142, 20);
+            this.txtLengthOfRectangle.TabIndex = 2;
+            // 
+            // txtWidthOfRectangle
+            // 
+            this.txtWidthOfRectangle.Location = new System.Drawing.Point(145, 60);
+            this.txtWidthOfRectangle.Name = "txtWidthOfRectangle";
+            this.txtWidthOfRectangle.Size = new System.Drawing.Size(142, 20);
+            this.txtWidthOfRectangle.TabIndex = 3;
+            // 
+            // lblOutputPerimeter
+            // 
+            this.lblOutputPerimeter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutputPerimeter.Location = new System.Drawing.Point(142, 172);
+            this.lblOutputPerimeter.Name = "lblOutputPerimeter";
+            this.lblOutputPerimeter.Size = new System.Drawing.Size(142, 20);
+            this.lblOutputPerimeter.TabIndex = 4;
+            this.lblOutputPerimeter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOutputArea
+            // 
+            this.lblOutputArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutputArea.Location = new System.Drawing.Point(145, 135);
+            this.lblOutputArea.Name = "lblOutputArea";
+            this.lblOutputArea.Size = new System.Drawing.Size(142, 20);
+            this.lblOutputArea.TabIndex = 5;
+            this.lblOutputArea.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOuputDiagonalLength
+            // 
+            this.lblOuputDiagonalLength.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOuputDiagonalLength.Location = new System.Drawing.Point(142, 210);
+            this.lblOuputDiagonalLength.Name = "lblOuputDiagonalLength";
+            this.lblOuputDiagonalLength.Size = new System.Drawing.Size(142, 20);
+            this.lblOuputDiagonalLength.TabIndex = 6;
+            this.lblOuputDiagonalLength.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(142, 97);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(142, 20);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnStartCalculator_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 255);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(142, 20);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(42, 255);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(142, 20);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblLengthInCentimeters
+            // 
+            this.lblLengthInCentimeters.AutoSize = true;
+            this.lblLengthInCentimeters.Location = new System.Drawing.Point(301, 27);
+            this.lblLengthInCentimeters.Name = "lblLengthInCentimeters";
+            this.lblLengthInCentimeters.Size = new System.Drawing.Size(21, 13);
+            this.lblLengthInCentimeters.TabIndex = 10;
+            this.lblLengthInCentimeters.Text = "cm";
+            // 
+            // lblWidthInCentimeters
+            // 
+            this.lblWidthInCentimeters.AutoSize = true;
+            this.lblWidthInCentimeters.Location = new System.Drawing.Point(301, 67);
+            this.lblWidthInCentimeters.Name = "lblWidthInCentimeters";
+            this.lblWidthInCentimeters.Size = new System.Drawing.Size(21, 13);
+            this.lblWidthInCentimeters.TabIndex = 11;
+            this.lblWidthInCentimeters.Text = "cm";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(97, 139);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(32, 13);
+            this.lblArea.TabIndex = 12;
+            this.lblArea.Text = "Area:";
+            this.lblArea.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Location = new System.Drawing.Point(75, 176);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(54, 13);
+            this.lblPerimeter.TabIndex = 13;
+            this.lblPerimeter.Text = "Perimeter:";
+            this.lblPerimeter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDiagonalLength
+            // 
+            this.lblDiagonalLength.AutoSize = true;
+            this.lblDiagonalLength.Location = new System.Drawing.Point(41, 210);
+            this.lblDiagonalLength.Name = "lblDiagonalLength";
+            this.lblDiagonalLength.Size = new System.Drawing.Size(88, 13);
+            this.lblDiagonalLength.TabIndex = 14;
+            this.lblDiagonalLength.Text = "Diagonal Length:";
+            this.lblDiagonalLength.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAreaInCentimeters
+            // 
+            this.lblAreaInCentimeters.AutoSize = true;
+            this.lblAreaInCentimeters.Location = new System.Drawing.Point(301, 139);
+            this.lblAreaInCentimeters.Name = "lblAreaInCentimeters";
+            this.lblAreaInCentimeters.Size = new System.Drawing.Size(0, 13);
+            this.lblAreaInCentimeters.TabIndex = 15;
+            // 
+            // lblPerimeterInCentimeters
+            // 
+            this.lblPerimeterInCentimeters.AutoSize = true;
+            this.lblPerimeterInCentimeters.Location = new System.Drawing.Point(301, 176);
+            this.lblPerimeterInCentimeters.Name = "lblPerimeterInCentimeters";
+            this.lblPerimeterInCentimeters.Size = new System.Drawing.Size(21, 13);
+            this.lblPerimeterInCentimeters.TabIndex = 16;
+            this.lblPerimeterInCentimeters.Text = "cm";
+            // 
+            // lblDiagonalLengthInCentimeters
+            // 
+            this.lblDiagonalLengthInCentimeters.AutoSize = true;
+            this.lblDiagonalLengthInCentimeters.Location = new System.Drawing.Point(301, 210);
+            this.lblDiagonalLengthInCentimeters.Name = "lblDiagonalLengthInCentimeters";
+            this.lblDiagonalLengthInCentimeters.Size = new System.Drawing.Size(21, 13);
+            this.lblDiagonalLengthInCentimeters.TabIndex = 17;
+            this.lblDiagonalLengthInCentimeters.Text = "cm";
+            // 
+            // frmRectangleCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 292);
+            this.Controls.Add(this.lblDiagonalLengthInCentimeters);
+            this.Controls.Add(this.lblPerimeterInCentimeters);
+            this.Controls.Add(this.lblAreaInCentimeters);
+            this.Controls.Add(this.lblDiagonalLength);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblWidthInCentimeters);
+            this.Controls.Add(this.lblLengthInCentimeters);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblOuputDiagonalLength);
+            this.Controls.Add(this.lblOutputArea);
+            this.Controls.Add(this.lblOutputPerimeter);
+            this.Controls.Add(this.txtWidthOfRectangle);
+            this.Controls.Add(this.txtLengthOfRectangle);
+            this.Controls.Add(this.lblWidthOfRectangle);
+            this.Controls.Add(this.lblLengthOfRectangle);
+            this.Name = "frmRectangleCalculator";
+            this.Text = "Rectangle Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLengthOfRectangle;
+        private System.Windows.Forms.Label lblWidthOfRectangle;
+        private System.Windows.Forms.TextBox txtLengthOfRectangle;
+        private System.Windows.Forms.TextBox txtWidthOfRectangle;
+        private System.Windows.Forms.Label lblOutputPerimeter;
+        private System.Windows.Forms.Label lblOutputArea;
+        private System.Windows.Forms.Label lblOuputDiagonalLength;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblLengthInCentimeters;
+        private System.Windows.Forms.Label lblWidthInCentimeters;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Label lblDiagonalLength;
+        private System.Windows.Forms.Label lblAreaInCentimeters;
+        private System.Windows.Forms.Label lblPerimeterInCentimeters;
+        private System.Windows.Forms.Label lblDiagonalLengthInCentimeters;
+    }
+}
+
---
+++ RectangleCalculator/frmRectangleCalculator.cs
@@ -0,0 +1,116 @@
+﻿// frmMRectangleCalculator.cs, RectangleCalculator.sln
+// CS 1181
+// Katherine Wilsdon
+// 7 September 2018
+// Dr. Cody Permann
+/* Description - Create a rectangle calculator. The user provides the length and
+ * width of the rectangle. The code calculates the perimeter, area, and diagonal 
+ * length of the rectangle and displays the results on the form.*/
+/* WOW:
+ * 1. Implement a Try-Catch statement to account for users inputting non-integers.
+ * 2. Implement an If--Elseif--Else statement to account for the user inputting zeros
+ * into the textboxes which would create nothing or a line.
+ * 3. Add a Clear button the clear the contents of the textboxes and output labels
+ * and put a focus in the first textbox.
+ * 4. Add a Exit button that closes the form.
+ * 5. Displays a message when the rectangle is a square.
+ * 6. The form starts the calculator when enter is pressed 
+ * via the AcceptButton under the form's properties */
+/* Works cited:
+  * Make centimeters squared: https://www.codeproject.com/Questions/502124/canplussomeoneplustellplusmeplushowplustopluswrite 
+  * AcceptButton: https://www.youtube.com/watch?v=seA5vjbKpxc 
+  * PerformClick: https://stackoverflow.com/questions/20626608/a-button-click-event-in-if-else-loop 
+  * Book, Chapters 3 and 4: Gaddis, T. (2017). Starting out with Visual C# (4th ed.). Boston, PA: Pearson.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RectangleCalculator {
+    public partial class frmRectangleCalculator : Form {
+        public frmRectangleCalculator() {
+            InitializeComponent();
+            // Make centimeter squared
+            lblAreaInCentimeters.Text = "cm\u00b2";
+        }
+
+        private void btnStartCalculator_Click(object sender, EventArgs e) {
+
+            try {
+                // Declare the datatypes of a variable.
+                int length; 
+                int width; 
+                int perimeter; 
+                int area; 
+                int line; 
+                double diagonalLength; 
+
+                //Store the inputs from the textboxes as length and width.
+                length = int.Parse(txtLengthOfRectangle.Text);
+                width = int.Parse(txtWidthOfRectangle.Text);
+                
+                // Displays a message to try again when length = width = 0 and clears the textboxes and putput labels.
+                if (length == 0 && width == 0) {
+                    MessageBox.Show("You have created nothing. Try again.");
+                    btnClear.PerformClick();
+                }
+                // Displays a message to try again when length = 0 or width = 0 and clears the textboxes and putput labels.
+                else if (length == 0 || width == 0) {
+                    line = length + width;
+                    MessageBox.Show("You have created a line that is " + line.ToString("n0") + " cm long. Try again.");
+                    btnClear.PerformClick();
+                }
+                else {
+                    // Calculates perimeter of the rectangle and displays the perimeter in the output label.
+                    perimeter = 2 * (length + width);
+                    lblOutputPerimeter.Text = perimeter.ToString("n0");
+
+                    // Calculates the area of the rectangle and displays the area in the output label.
+                    area = length * width;
+                    lblOutputArea.Text = area.ToString("n0");
+
+                    /* Calculates the diagonal length of a rectangle and displays the diagonal length 
+                     * in the output label rounded to two decimal points. */
+                    diagonalLength = Math.Pow(length, 2) + Math.Pow(width, 2);
+                    diagonalLength = Math.Sqrt(diagonalLength);
+                    lblOuputDiagonalLength.Text = diagonalLength.ToString("n2");
+
+                    // Displays a message when the rectangle is a square.
+                    if (length == width) {
+                        MessageBox.Show("This rectangle is a square.");
+                    }
+                }
+
+            }
+            catch (Exception ex) {
+                // Display the default error message and clears the textboxes and output labels. 
+                MessageBox.Show(ex.Message);
+                btnClear.PerformClick();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            // Closes the form.
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) {
+            // Clears the text in the textboxes and output labels.
+            txtLengthOfRectangle.Text = "";
+            txtWidthOfRectangle.Text = "";
+            lblOutputPerimeter.Text = "";
+            lblOutputArea.Text = "";
+            lblOuputDiagonalLength.Text = "";
+
+            // Puts a focus in the txtLengthOfRectangle
+            txtLengthOfRectangle.Focus();
+
+        }
+    }
+}
